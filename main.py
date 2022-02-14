508b09f32dbaecc5c1310f360242c7f6fd3fe09b
violetakokorevi <91872388+violetakokorevicha@users.noreply.github.com>
2022-02-14 08:05:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+n = int(input('Your number:'))
+if n <= 99:
+  print('2 digit word')
+if n >= 100:
+  print('3 digit word')
+if n % 2 == 0:
+    print('even')
+else:
+    print('odd')
+if n > 0:
+    print('pozitive')
+else:
+    print('negative')
